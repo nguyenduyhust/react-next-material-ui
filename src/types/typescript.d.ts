fb5aeb35de22a1a88956e7ae32674f58e34935fb
Duy Nguyen <nguyenduy.hust@gmail.com>
2021-01-18 16:41:59 +0700

    Update typescript.d.ts

--- src/types/typescript.d.ts
+++ src/types/typescript.d.ts
@@ -1,11 +1,11 @@
-export type Overwrite<Base1, Base2> = {
+type Overwrite<Base1, Base2> = {
   [Key in Exclude<keyof Base1, keyof Base2>]: Base1[Key]
 } & Base2;
 
-export type PickWithType<Base, Condition> = Pick<Base, {
+type PickWithType<Base, Condition> = Pick<Base, {
   [Key in keyof Base]: Condition extends Extract<Base[Key], Condition> ? Key : never;
 }[keyof Base]>;
 
-export type Optional<Base, OptionalKey extends keyof Base> = Pick<Base, Exclude<keyof Base, OptionalKey>> & {
+type Optional<Base, OptionalKey extends keyof Base> = Pick<Base, Exclude<keyof Base, OptionalKey>> & {
   [Key in OptionalKey]?: Base[Key];
 };
